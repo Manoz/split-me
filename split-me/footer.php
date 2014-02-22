027ab620900bb9a7d204b8679e5843024774834a
Manoz <manoz@outlook.com>
2014-02-22 12:22:13 +0100

    Rem caffeine link

--- split-me/footer.php
+++ split-me/footer.php
@@ -9,7 +9,6 @@
     </div>
     <footer class="sm-f">
         <small class="copyright">&copy; <?php echo date( 'Y' ); ?> - <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo('name'); ?>" ><?php bloginfo('name'); ?></a></small>
-        <small class="credits"><i class="fa fa-coffee wiggle"></i> Fueled with <a title="Caffeine" target="_blank" href="https://en.wikipedia.org/wiki/Caffeine">C<sub>8</sub>H<sub>10</sub>N<sub>4</sub>O<sub>2</sub></a>.</small>
     </footer>
 
 
